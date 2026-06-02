6ef0effae0888f93d82e6fa5212e4e062fa7b280
mcclusky <samantharobertz@outlook.com>
2026-06-02 15:07:39 +0700

    Update ubuntusysteminstall.sh

--- ubuntusysteminstall.sh
+++ ubuntusysteminstall.sh
@@ -7,44 +7,44 @@
 #Autopurge unnecessary packages
 #Defragment system to prevent system file corruption
 
-sudo add-apt-repository ppa:zhangsongcui3371/fastfetch
-sudo apt update
+sudo add-apt-repository ppa:zhangsongcui3371/fastfetch -y
+sudo apt update -y
 
 # Removing unnecessary packages from the system
-sudo apt autopurge kasumi okular 
-sudo apt autopurge gnome-keyring libpam-gnome-keyring 
-sudo apt autopurge firefox thunderbird libreoffice-common
-sudo apt autopurge timeshift celluloid rhythmbox
-sudo apt autopurge libreoffice-uiconfig-common libreoffice-uiconfig-writer libreoffice-uiconfig-calc libreoffice-uiconfig-impress libreoffice-uiconfig-draw
-sudo apt autopurge gnome-clocks gnome-calendar gnome-calculator gnome-text-editor eog gnome-screenshot gnome-software
-sudo apt autopurge evince
-sudo apt autopurge xviewer xreader xed
-sudo apt autopurge hypnotix
-sudo apt autopurge drawing
-sudo apt autopurge kwrite gwenview kontact kclock kate kde-spectacle
-sudo apt autopurge snapd
-sudo apt autopurge imagemagick-6-common
-sudo apt autopurge mintinstall
-sudo apt autopurge plasma-discover ubuntu-mate-welcome lite-software
-sudo apt autopurge ubuntu-report whoopsie
+sudo apt autopurge kasumi okular -y
+sudo apt autopurge gnome-keyring libpam-gnome-keyring -y
+sudo apt autopurge firefox thunderbird libreoffice-common -y
+sudo apt autopurge timeshift celluloid rhythmbox -y
+sudo apt autopurge libreoffice-uiconfig-common libreoffice-uiconfig-writer libreoffice-uiconfig-calc libreoffice-uiconfig-impress libreoffice-uiconfig-draw -y
+sudo apt autopurge gnome-clocks gnome-calendar gnome-calculator gnome-text-editor eog gnome-screenshot gnome-software -y
+sudo apt autopurge evince -y
+sudo apt autopurge xviewer xreader xed -y
+sudo apt autopurge hypnotix -y
+sudo apt autopurge drawing -y
+sudo apt autopurge kwrite gwenview kontact kclock kate kde-spectacle -y
+sudo apt autopurge snapd -y
+sudo apt autopurge imagemagick-6-common -y
+sudo apt autopurge mintinstall -y
+sudo apt autopurge plasma-discover ubuntu-mate-welcome lite-software -y
+sudo apt autopurge ubuntu-report whoopsie -y
 
 #Installing essential packages for the system, for a more complete experience (for items missing due to removal, you need to install the Flathub variant of your choice via the newly installed EasyFlatpak store!)
-sudo apt install fcitx5 fcitx5-unikey bleachbit kazam python3-pip yt-dlp fastfetch spice-vdagent flatpak software-properties-common software-properties-gtk
+sudo apt install fcitx5 fcitx5-unikey bleachbit kazam python3-pip yt-dlp fastfetch spice-vdagent flatpak software-properties-common software-properties-gtk -y
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
 
 
-sudo apt update
-sudo apt --fix-broken install
-sudo apt full-upgrade
-sudo apt install --reinstall dolphin
+sudo apt update -y
+sudo apt --fix-broken install -y
+sudo apt full-upgrade -y
 
-flatpak install flathub org.dupot.easyflatpak
-flatpak install flathub io.github.obiwankennedy.HotShots
+
+flatpak install -y flathub org.dupot.easyflatpak
+flatpak install -y flathub io.github.obiwankennedy.HotShots
 
 #Force Wayland to use Kazam to take screenshots of your computer
 gsettings set org.gnome.shell disable-extension-version-validation true
 
 # Defragmenting the system to increase performance, then refreshing the system
-sudo apt autopurge
-sudo fstrim -av
+sudo apt autopurge -y
+sudo e4defrag /home 
